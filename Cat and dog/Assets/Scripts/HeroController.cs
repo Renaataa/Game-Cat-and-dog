c51b0b5ce7570c3e9df76db7fab10619ef7127b8
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-02-11 17:50:32 +0100

    Cat jumping and patches.Transitions

--- Cat and dog/Assets/Scripts/HeroController.cs
+++ Cat and dog/Assets/Scripts/HeroController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class HeroController : MonoBehaviour
 {
     Animator anim;
+    Rigidbody2D body;
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
         
     }
 
@@ -19,6 +22,7 @@ public class HeroController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             anim.SetTrigger("Jump");
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
