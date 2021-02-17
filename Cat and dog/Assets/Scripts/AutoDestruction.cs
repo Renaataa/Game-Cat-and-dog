e4c669822aaae06ab472fcf6378326bfc69c5774
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-02-17 17:46:52 +0100

    Objects disappearing outside the camera

---
+++ Cat and dog/Assets/Scripts/AutoDestruction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestruction : MonoBehaviour
+{
+    public float timeToDestruction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, timeToDestruction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
