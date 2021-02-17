548ea4730bf0dae298b048e7b97bf8c493338305
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-02-17 18:48:57 +0100

    Randomness of the ground position

--- Cat and dog/Assets/Scripts/GroundGenerator.cs
+++ Cat and dog/Assets/Scripts/GroundGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GroundGenerator : MonoBehaviour
 {
     public GameObject groundPrefab;
+    public float maxStep;
+    private float previousGroundY = -2.38f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,8 @@ public class GroundGenerator : MonoBehaviour
 
     void GenerateNewGround()
     {
-        Instantiate(groundPrefab, this.transform.position, Quaternion.identity);
+        Vector3 targetPrefabPosition = new Vector3(this.transform.position.x, Random.Range(-3f, maxStep + previousGroundY), this.transform.position.z);
+        previousGroundY = targetPrefabPosition.y;
+        Instantiate(groundPrefab, targetPrefabPosition, Quaternion.identity);
     }
 }
