fe8ab26f4978b6b9ed26904ccf4fd3e2e3c0af16
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-02-17 16:47:36 +0100

    Substrate generator
    Change Unity version (2019.4.14f1 -> 2019.4.20f1)

---
+++ Cat and dog/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    public GameObject groundPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GenerateNewGround", 0, 2.6f);
+    }
+
+    void GenerateNewGround()
+    {
+        Instantiate(groundPrefab, this.transform.position, Quaternion.identity);
+    }
+}
