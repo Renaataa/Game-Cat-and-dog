a737afd3c38ef7f1cdc9c6eaaf867ff20b343131
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-04 21:18:07 +0100

    Smooth flight of the balloon

--- Cat and dog/Assets/Scripts/SymmetricXPosition.cs
+++ Cat and dog/Assets/Scripts/SymmetricXPosition.cs
@@ -6,15 +6,28 @@ public class SymmetricXPosition : MonoBehaviour
 {
     public GameObject hero;
 
+    Vector3 currentPosition;
+    Vector3 targetPosition;
+
+    float startTime;
+    private float currentTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("setNewPositions", 0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.position = new Vector3 ( Mathf.Abs(hero.transform.position.x), transform.position.y, transform.position.z);
+        currentTime = Time.time - startTime;
+        transform.position = Vector3.Slerp(currentPosition, targetPosition, currentTime);
+    }
+
+    void setNewPositions(){
+        currentPosition = transform.position;
+        targetPosition = new Vector3 (Mathf.Abs(hero.transform.position.x), transform.position.y, transform.position.z);
+        startTime = Time.time;
     }
 }
