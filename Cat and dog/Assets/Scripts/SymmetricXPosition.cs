9dbe65312a599465181ecd563fcf7a20bb79c86d
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-04 20:24:23 +0100

    The balloon we're trying to catch up with

---
+++ Cat and dog/Assets/Scripts/SymmetricXPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymmetricXPosition : MonoBehaviour
+{
+    public GameObject hero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3 ( Mathf.Abs(hero.transform.position.x), transform.position.y, transform.position.z);
+    }
+}
