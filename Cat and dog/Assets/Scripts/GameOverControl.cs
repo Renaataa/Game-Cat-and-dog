98a6928b23e93b9a36896d73742184439fac3aff
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-06 11:29:45 +0100

    Limitation of space for the cat; Detecting success and failure

---
+++ Cat and dog/Assets/Scripts/GameOverControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "ballon"){
+            Debug.Log("Sukces!");
+        }
+        else if(other.gameObject.tag == "border"){
+            Debug.Log("Porazka.");
+        }
+    }
+}
