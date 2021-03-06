813f874979141c44074b64e0249c568ed8b7b83a
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-06 12:09:09 +0100

    Displaying panels for success and failure

--- Cat and dog/Assets/Scripts/GameOverControl.cs
+++ Cat and dog/Assets/Scripts/GameOverControl.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class GameOverControl : MonoBehaviour
 {
+    private GameObject successPanel;
+    private GameObject failurePanel;
+    private GameObject gameOverCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverCanvas = GameObject.Find("GameOverCanvas");
+        if(gameOverCanvas != null){
+            failurePanel = gameOverCanvas.transform.GetChild(0).gameObject;
+            successPanel = gameOverCanvas.transform.GetChild(1).gameObject;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.tag == "ballon"){
             Debug.Log("Sukces!");
+            successPanel.SetActive(true);
         }
         else if(other.gameObject.tag == "border"){
             Debug.Log("Porazka.");
+            failurePanel.SetActive(true);
         }
     }
 }
