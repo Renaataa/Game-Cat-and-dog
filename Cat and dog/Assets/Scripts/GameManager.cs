8fabf40e33b29e6e8e6c8654a8b397046bc3434a
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-08 14:46:05 +0100

    Function that loads levels

--- Cat and dog/Assets/Scripts/GameManager.cs
+++ Cat and dog/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    public int currentLevel;
+    public string[] levelNames;
 
     void Awake(){
         if(instance == null){
@@ -23,9 +26,8 @@ public class GameManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenLevel(int newLevelNum)
     {
-        
+        SceneManager.LoadScene(levelNames[newLevelNum]);
     }
 }
