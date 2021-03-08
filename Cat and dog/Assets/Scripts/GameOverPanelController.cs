3f7acd1428d8beea447813c4465bde65a3459fd2
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-08 15:19:10 +0100

    Using Game Manager.
    Support for buttons 'Try again' and 'Next Level'

---
+++ Cat and dog/Assets/Scripts/GameOverPanelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPanelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadLevelAgain(){
+        Debug.Log(GameManager.instance.currentLevel);
+        GameManager.instance.OpenLevel(GameManager.instance.currentLevel);
+    }
+    
+    public void LoadNextLevel(){
+        GameManager.instance.OpenLevel(GameManager.instance.currentLevel+1);
+    }
+}
