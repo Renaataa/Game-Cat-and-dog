1fc32dfbf81d73f748e7a51cac9e4853ca5dfddb
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-08 15:46:47 +0100

    Fix. Add button ReturnToMenu

--- Cat and dog/Assets/Scripts/GameOverPanelController.cs
+++ Cat and dog/Assets/Scripts/GameOverPanelController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverPanelController : MonoBehaviour
 {
@@ -16,6 +17,18 @@ public class GameOverPanelController : MonoBehaviour
     }
     
     public void LoadNextLevel(){
-        GameManager.instance.OpenLevel(GameManager.instance.currentLevel+1);
+        GameManager.instance.currentLevel++;
+        if(GameManager.instance.currentLevel < GameManager.instance.levelNames.Length){
+            GameManager.instance.OpenLevel(GameManager.instance.currentLevel);
+        }
+        else{
+            GameManager.instance.currentLevel = 0;
+            GameManager.instance.OpenLevel(GameManager.instance.currentLevel);
+        }
+        
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("menu");
     }
 }
